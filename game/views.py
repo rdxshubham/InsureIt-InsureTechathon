5273ab16894fa42827aa092bae65063c07e5b003
Shubham Gupta <AVI2147520@maricopa.edu>
2017-12-09 20:15:57 +0530

    Static folder added with content and view modification

--- game/views.py
+++ game/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic import View
+
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, template_name="index.html")
